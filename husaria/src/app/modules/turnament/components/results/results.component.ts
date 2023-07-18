f4c8bfe25e933caf426302221f904525ea05eac3
michalc10 <michalc1101@gmail.com>
2023-07-18 07:34:06 +0200

    export pdf

--- husaria/src/app/modules/turnament/components/results/results.component.ts
+++ husaria/src/app/modules/turnament/components/results/results.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import * as FileSaver from 'file-saver';
 import { IPlayerPoints } from 'src/app/models/playerPoints';
 import { PlayerPointsService } from '../../services/playerPoints/playerPoints.service';
-
-export interface Column {
-  field: string;
-  header: string;
-  customExportHeader?: string;
-}
-
-export interface ExportColumn {
-  title: string;
-  dataKey: string;
-}
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { style } from '@angular/animations';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-results',
@@ -26,7 +18,6 @@ export class ResultsComponent implements OnInit {
   selectedPlayerId = '-1';
   participantList: IPlayerPoints[] = [];
   tournamentId = "-1"
-  exportColumns!: ExportColumn[];
   constructor(
     private playerPointsService: PlayerPointsService
   ) { }
@@ -43,34 +34,79 @@ export class ResultsComponent implements OnInit {
         }).sort((a, b) => { return a.score - b.score });
       },
     })
-    this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
+    // this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
   }
 
   exportPdf() {
-    import('jspdf').then((jsPDF) => {
-        import('jspdf-autotable').then((x) => {
-            const doc = new jsPDF.default('p', 'px', 'a4');
-            (doc as any).autoTable(this.exportColumns, this.participantList);
-            doc.save('products.pdf');
-        });
-    });
-}
+    // import('jspdf').then((jsPDF) => {
+    //   import('jspdf-autotable').then((x) => {
+    //     const doc = new jsPDF.default('p', 'px', 'a4');
+    //     (doc as any).autoTable(this.exportColumns, this.participantList);
+    //     doc.save('products.pdf');
+    //   });
+    // });
+    const tableBody = this.buildTableBody(this.participantList);
 
-exportExcel() {
-    import('xlsx').then((xlsx) => {
-        const worksheet = xlsx.utils.json_to_sheet(this.participantList);
-        const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-        this.saveAsExcelFile(excelBuffer, 'products');
-    });
-}
+    const documentDefinition = {
+      pageOrientation: 'landscape' as const,
+      content: [
+        {
+          table: {
+            headerRows: 1,
 
-saveAsExcelFile(buffer: any, fileName: string): void {
-    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    let EXCEL_EXTENSION = '.xlsx';
-    const data: Blob = new Blob([buffer], {
-        type: EXCEL_TYPE
+            body: [
+              [
+                { text: '#', bold: true },
+                { text: 'Husarz', bold: true },
+                { text: 'Koń', bold: true },
+                { text: 'Chorągiew', bold: true },
+                { text: 'Wynik', bold: true }
+              ],
+
+              ...tableBody
+            ]
+          }
+        }
+      ],
+      defaultStyle: {
+        fontSize: 15,
+        // bold: true
+      }
+    };
+
+    pdfMake.createPdf(documentDefinition).open();
+
+  }
+
+  // exportExcel() {
+  //     import('xlsx').then((xlsx) => {
+  //         const worksheet = xlsx.utils.json_to_sheet(this.participantList);
+  //         const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+  //         const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+  //         this.saveAsExcelFile(excelBuffer, 'products');
+  //     });
+  // }
+
+  // saveAsExcelFile(buffer: any, fileName: string): void {
+  //     let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  //     let EXCEL_EXTENSION = '.xlsx';
+  //     const data: Blob = new Blob([buffer], {
+  //         type: EXCEL_TYPE
+  //     });
+  //     FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  // }
+
+
+
+  buildTableBody(data: IPlayerPoints[]) {
+    const body: any[] = [];
+
+    let i =1
+    data.forEach(row => {
+      body.push([i,row.playerName, row.horse, row.flag, row.score]);
+      i++;
     });
-    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-}
+
+    return body;
+  }
 }
