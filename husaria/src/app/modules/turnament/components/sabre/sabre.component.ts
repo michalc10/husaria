ccd4c9301482d74644e95263eaba340b4260555b
michalc10 <michalc1101@gmail.com>
2023-08-17 17:10:14 +0200

    generate sabre pdf

--- husaria/src/app/modules/turnament/components/sabre/sabre.component.ts
+++ husaria/src/app/modules/turnament/components/sabre/sabre.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerPointsService } from '../../services/playerPoints/playerPoints.service';
 import { Location } from '@angular/common';
 import { IPlayerPoints } from 'src/app/models/playerPoints';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { PageOrientation } from 'pdfmake/interfaces';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-sabre',
@@ -10,14 +14,17 @@ import { IPlayerPoints } from 'src/app/models/playerPoints';
 })
 export class SabreComponent implements OnInit {
 
-  selectedPlayerId='-1';
+  selectedPlayerId = '-1';
   points = [6, 6, 10, 25, 25, 5, 25, 25, 6, 5, 20, 40];
   participantList: IPlayerPoints[] = [];
+  tournamentId = "-1"
+
+  isOpenedPdfMakeDialog = false;
+
   constructor(
     private playerPointsService: PlayerPointsService,
     private location: Location
   ) { }
-  tournamentId = "-1"
 
   ngOnInit() {
     this.tournamentId = this.location.path().split('/')[2];
@@ -39,7 +46,7 @@ export class SabreComponent implements OnInit {
 
     participant.sabreScore += value === 1 ? this.points[index] : -this.points[index];
 
-   
+
     this.updatePlayerPoints(participant, participantIndex)
   }
 
@@ -55,7 +62,7 @@ export class SabreComponent implements OnInit {
     }
 
     participant.sabreScore = score + ev;
-   
+
     this.updatePlayerPoints(participant, participantIndex)
   }
 
@@ -64,11 +71,96 @@ export class SabreComponent implements OnInit {
     console.log(this.selectedPlayerId)
   }
 
-  updatePlayerPoints(participant:IPlayerPoints,participantIndex:number){
+  updatePlayerPoints(participant: IPlayerPoints, participantIndex: number) {
     this.playerPointsService.update(participant._id!, participant).subscribe({
       next: (value) => {
         this.participantList[participantIndex] = value;
       },
     })
   }
+
+  closePdfMekeDialog(ev: any) {
+    this.isOpenedPdfMakeDialog = false;
+  }
+
+
+
+  generatePDF() {
+    const tableBody = this.participantList.map(player => {
+      const row = [
+        { text: player.playerName, style: ['line-left', 'line-right', 'center-text'] },
+        // ... Map other cell values here
+      ];
+
+      for (const point of this.points) {
+        row.push({
+          text: player.sabrePoints.charAt(point) === '0' ? '✔' : '✘',
+          style: ['line-left', 'line-right', 'center-text']
+        });
+      }
+
+      row.push(
+        { text: player.sabreTime.toFixed(2), style: ['line-left', 'line-right', 'center-text'] },
+        { text: player.sabreScore.toFixed(3), style: ['line-left', 'line-right', 'center-text'] }
+      );
+
+      return row;
+    });
+
+
+
+    const docDefinition = {
+      pageOrientation: 'landscape' as PageOrientation,
+      content: [
+        { width: '*', text: '' },
+        {
+          style: 'table',
+          width: 'auto',
+          table: {
+            headerRows: 3,
+            body: [
+              ['', '', 'P1', 'P2', '', 'P3 beczka', '', '', "P4 skok", '', 'P5', '', 'Punkty karne', '', '', ''],
+              ['Start', 'Imię', 'Cięcie łozy', 'Cięcie jabłka', 'Niższy chód', 'Ominięcie przeszkody', 'Demontaż przeszkody', 'Zrzutka', 'Ominięcie przeszkody', 'Demontaż przeszkody', 'Cięcie jabłka', 'Utrata broni', 'Upadek jeźdźca', 'Upadek konia i jeźdźca', 'Czas [s]', 'Wynik'],
+              ['', '', ...this.points, '', ''],
+              ...this.participantList.map((player, rowIndex) => {
+                const saberPoints = [...player.sabrePoints].map(point => { return point === '0' ? '' : 'X' })//'✔' : '✘'
+                return [
+                  rowIndex + 1,
+                  player.playerName,
+                  ...saberPoints,
+                  player.sabreTime,
+                  player.sabreScore
+                ]
+              })
+            ]
+          },
+        },
+      ],
+      styles: {
+        // Define your styles here
+        // table: { alignment:'center' },
+        lineLeft: { fillColor: 'lightcyan', border: [true, true, false, true] },
+        lineRight: { fillColor: 'lightcyan', border: [false, true, true, true] },
+        lineBold: { fillColor: 'lightcyan', border: [true, true, true, true], lineWidth: 2 },
+        // cell: { fillColor: 'lightcyan', alignment: 'center', fontSize: 10 },
+        // anotherStyle: {   alignment: 'right', fontSize: 12 },
+        // subHeader: { bold: true, alignment: 'center', fontSize: 10 },
+        // centerText: { alignment: 'center', verticalAlignment: 'middle' },
+        // Define other styles here,
+        // anotherStyle: {
+        //   italics: true,
+        //   alignment: 'right'
+        // }
+      },
+      defaultStyle: { fontSize: 10 },
+    };
+
+    // docDefinition.content[0].table.body.push(...tableBody);
+
+    pdfMake.createPdf(docDefinition).open();
+  }
+
+
+
+
 }
